8bad86f42c17bcb1a0174ae52e5bcd8ff9cf28a6
santiagocastillo1322 <licenciadoflowelflow@gmail.com>
2025-05-31 02:54:31 -0500

    Instalado react-select para solucionar error de build

--- src/pages/Formestudiante.jsx
+++ src/pages/Formestudiante.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Select from "react-select";
+
 
 const habilidadesOptions = [
   { value: "programacion", label: "Programación" },
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // 👈 Esto es clave para que el build funcione bien en Vercel si usas rutas relativas
   plugins: [react()],
   css: {
-    postcss: "./postcss.config.js", // Asegura que PostCSS se use correctamente
+    postcss: "./postcss.config.js",
   },
 });
